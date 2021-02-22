6a7a041e4f8aeaf11e33b2f28f0b969f81e86a95
psychomugs <psychomugs@gmail.com>
2021-02-22 00:02:40 -0500

    Add progress messages

--- dailycheck.py
+++ dailycheck.py
@@ -20,6 +20,7 @@ d = wd.Chrome(
 d.get('https://dailycheck.cornell.edu/saml_login_user?redirect=%2Fdaily-checkin')
 
 # login with netID and password
+print("Logging in...")
 d.find_elements_by_id('netid')[0].send_keys(args['netid'])
 d.find_elements_by_id('password')[0].send_keys(args['password'])
 d.find_elements_by_class_name('input-submit')[0].click()
@@ -27,6 +28,7 @@ time.sleep(1)
 
 try:
     # proceed to form
+    print("Opening form...")
     c = d.find_elements_by_id('continue')[0]
     c.click()
 
@@ -37,10 +39,12 @@ try:
         'exposure',
         'positivetestever'
     ]
+    print("Clicking buttons...")
     for radio in radios:
         d.find_elements_by_id(radio+'-no')[0].click()
 
     # submit twice
+    print("Submitting...")
     for _ in range(2):
         d.find_elements_by_id('submit')[0].click()
         time.sleep(1)
@@ -53,6 +57,6 @@ try:
     status_msg = status_msg[:status_msg.find('<')]+status_msg[status_msg.find('>')+1:]
     print("Status message: ", status_msg)
 except:
-    print("Error logging in and completing form, no status message found")
+    print("Error logging in and completing form, no status message found.")
 
 d.close()
